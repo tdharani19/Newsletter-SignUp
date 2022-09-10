091b015b1bf9737747e41b4a2a734166c1e89924
Dharani T <105157784+tdharani19@users.noreply.github.com>
2022-09-10 22:31:48 +0530

    Add files

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+   res.sendFile(__dirname + "/signup.html");
+});
+app.post("/", function(req, res){
+
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us18.api.mailchimp.com/3.0/lists/ea5731f4b4";
+  const options = {
+    method:"POST",
+    auth: "dharani:4cdae0610a79ead589242b688ebcf109-us18"
+  }
+
+
+  const request = https.request(url, options, function(response){
+   if(response.statusCode === 200){
+     res.sendFile(__dirname + "/success.html");
+
+   } else {
+     res.sendFile(__dirname + "/failure.html");
+   }
+
+
+   response.on("data", function(data){
+     console.log(JSON.parse(data));
+   })
+ })
+
+ request.write(jsonData);
+ request.end();
+
+});
+
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("server started");
+});
+
+
+//4cdae0610a79ead589242b688ebcf109-us18 API Key
+//ea5731f4b4 audience id
